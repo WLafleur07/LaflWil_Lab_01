63cd9d718abe96feb61cd3f99e69d283520d57a5
William Lafleur <williamlafleur@hotmail.ca>
2022-01-18 14:32:06 -0500

    Added a java comment instead of a line comment

--- src/Lab/HelloWorld.java
+++ src/Lab/HelloWorld.java
@@ -1,4 +1,4 @@
-/*
+/**
 *   William Lafleur
 *   Date: January 18th, 2022
 *   Description: Rudimentary program to try printing from a main function.
@@ -8,7 +8,10 @@
 package Lab;
 
 public class HelloWorld { // public class which matches the file name, to allow the code to run
-    public static void main(String[] args) // method to run the program
+    /**
+     * Main method is used to run the program
+     */
+    public static void main(String[] args)
     {
         System.out.println("hello world!"); // print function with build in new line
         System.out.print("this is for CS321\n"); // standard print method

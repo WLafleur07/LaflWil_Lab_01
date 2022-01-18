36105626f67216949ab73362a65aa6d89a027cf9
William Lafleur <williamlafleur@hotmail.ca>
2022-01-18 11:41:35 -0500

    Committing the finalised Lab_01

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"Lab","c":"HelloWorld","l":"HelloWorld()","u":"%3Cinit%3E()"},{"p":"Lab","c":"HelloWorld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ src/Lab/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+*   William Lafleur
+*   Date: January 18th, 2022
+*   Description: Rudimentary program to try printing from a main function.
+*/
+
+
+package Lab;
+
+public class HelloWorld { // public class which matches the file name, to allow the code to run
+    public static void main(String[] args) // method to run the program
+    {
+        System.out.println("hello world!"); // print function with build in new line
+        System.out.print("this is for CS321\n"); // standard print method
+        System.out.print("glad to see it working");
+    }
+}
